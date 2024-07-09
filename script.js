d782f681962ad218b7cab232528af89463ba7db3
Yuki1511 <t.a.ug.b.a.6789c@gmail.com>
2024-07-09 14:59:31 +0900

    修正

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const A = document.querySelector('#A');
 
-let count = 0;
 // マスをクリックしたら、○を表示
 A.addEventListener('click', () => {
     count++;
     // 奇数偶数の判定
-    if (count%2===1) {
+    if (count % 2 === 1) {
         A.textContent = '○'
     } else {
         A.textContent = '×'
@@ -14,20 +13,31 @@ A.addEventListener('click', () => {
 
 // 全マスにイベント設定
 // 配列を作る？
-const masu =  document.querySelectorAll('.board div');
+const masu = document.querySelectorAll('.board div');
 console.log(masu);
 
-for(let i = 0; i < 9; i++) {
-    masu[0].addEventListener('click', () => {
+let count = 0;
+
+for (let i = 0; i < 9; i++) {
+    masu[i].addEventListener('click', () => {
         count++;
         // 奇数偶数の判定
-        if (count%2===1) {
-            masu[0].textContent = '○'
+        if (count % 2 === 1) {
+            masu[i].textContent = '○'
+            masu[i].classList.add('maru');
         } else {
-            masu[0].textContent = '×'
+            masu[i].textContent = '×'
+            masu[i].classList.add('batu');
         }
+        if (masu[0].textContent === '○' &&
+            masu[1].textContent === '○' &&
+            masu[2].textContent === '○'
+        ) {
+            console.log(masu[0].textContent)
+            console.log('○の勝ち！')
+        }
+
     })
 }
 
 // なんでfor文
-
